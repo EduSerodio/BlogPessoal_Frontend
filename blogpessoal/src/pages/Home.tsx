13c1c2da12ae377c7503c41c9626a5ef50f3af84
EduSerodio <dudu9090@outlook.com.br>
2023-10-24 08:10:38 -0300

    Iniciando o  react no projeto

---
+++ blogpessoal/src/pages/Home.tsx
@@ -0,0 +1,32 @@
+import './Home.css'
+
+function Home() {
+  return (
+    <>
+        <header>
+
+        <div className='cabecalhoSuperior'>
+            <h1>BLOG PESSOAL</h1>
+            <ul>
+                <li><a href="#">Postagens</a></li>
+                <li><a href="#">Temas</a></li>
+                <li><a href="#">Cadastrar tema</a></li>
+                <li><a href="#">Perfil</a></li>
+                <li><a href="#">Sair</a></li>
+            </ul>
+        </div>
+
+        <div className='cabecalhoInferior'>
+            <p>Lorem ipsum</p>
+
+            <p>adadadadadaddasdasd</p>
+        </div>
+        
+        </header>
+        
+        
+        <h2>Começando o projeto do blog pessoal!</h2>
+    </>
+  )
+}
+export default Home
