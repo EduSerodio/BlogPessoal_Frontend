19c7c77b06e2c62eaad672e02520dcc5c525ffff
EduSerodio <dudu9090@outlook.com.br>
2023-10-26 12:24:10 -0300

    interação da aplicação do front com o back end

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { AuthProvider } from './contexts/AuthContext';
 
 import Home from './pages/home/Home';
 import Login from './pages/login/Login';
@@ -7,23 +8,21 @@ import Navbar from './components/navbar/Navbar';
 import Cadastro from './pages/cadastro/Cadastro';
 
 function App() {
-  return (
-    <>
-      <BrowserRouter>
-        <Navbar />
-          <div className='min-h-[80vh]'>
-            <Routes>
-              <Route path="/" element={<Login />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/home" element={<Home />} />
-              <Route path="/cadastro" element={<Cadastro />} />
-            </Routes>
-          </div>  
-        <Footer />
-      </BrowserRouter>
-    </>
-  );
+    return (
+        <AuthProvider>
+            <BrowserRouter>
+                <Navbar />
+                <div className='min-h-[80vh]'>
+                    <Routes>
+                        <Route path="/" element={<Login />} />
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/home" element={<Home />} />
+                        <Route path="/cadastro" element={<Cadastro />} />
+                    </Routes>
+                </div>
+                <Footer />
+            </BrowserRouter>
+        </ AuthProvider>
+    );
 }
-
 export default App;
-
